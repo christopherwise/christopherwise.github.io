adc9a285bb0d10252f91ebaf9a5a88b08ea8538f
Christopher Wise <wise@uber.com>
2017-05-16 22:41:17 -0700

    Updating site to have mobile nav

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 /* slideshow */
-$(document).ready(function() {
-function play() {
-    setInterval(function(){
-        var next = $(".slideshow .active").removeClass("active").next(".image");
-        if (!next.length) {
-            next = $(".slideshow .image:first");
-        }
-        next.addClass("active");
-    }, 15000);
-}
-play();
-});
+// $(document).ready(function() {
+// function play() {
+//     setInterval(function(){
+//         var next = $(".slideshow .active").removeClass("active").next(".image");
+//         if (!next.length) {
+//             next = $(".slideshow .image:first");
+//         }
+//         next.addClass("active");
+//     }, 15000);
+// }
+// play();
+// });
 
 /* nav */
 
@@ -18,9 +18,12 @@ $(window).scroll(function() {
 
     if ($(window).scrollTop() > 50) {
         $('nav').addClass('follow black');
+        $('.icon-left, .icon-right').addClass('backBlack');
+
        
     } else {
         $('nav').removeClass('follow black');
+        $('.icon-left, .icon-right').removeClass('backBlack');
         
     }
 });
@@ -46,21 +49,37 @@ $(window).scroll(function() {
 
     });
 
-$(function(){
-    $(".wine").typed('reset')
-      $(".wine").typed({
-            strings: ["I also like wine."],
-            typeSpeed: 20,
-            backDelay: 500,
-            // time before typing starts
-            startDelay: 16000,
-            loop: false,
-            // show cursor
-            showCursor: true,
-            // character for cursor
-            cursorChar: "|",
-            contentType: 'html', // or text
-            // defaults to false for infinite loop
-            loopCount: false,
-        });
+// $(function(){
+//     $(".wine").typed('reset')
+//       $(".wine").typed({
+//             strings: ["I also like wine."],
+//             typeSpeed: 20,
+//             backDelay: 500,
+//             // time before typing starts
+//             startDelay: 16000,
+//             loop: false,
+//             // show cursor
+//             showCursor: true,
+//             // character for cursor
+//             cursorChar: "|",
+//             contentType: 'html', // or text
+//             // defaults to false for infinite loop
+//             loopCount: false,
+//         });
+// });
+
+// open mobile menu
+$('.js-toggle-menu').click(function(e){
+  e.preventDefault();
+  $('.mobile-header-nav').slideToggle();
+  $(this).toggleClass('open');
+});
+
+// menu
+$('.btn1').click(function(){
+    $('.menu').toggleClass("show");
+});
+
+$('.btn1').click (function(){
+  $(this).toggleClass('open');
 });
